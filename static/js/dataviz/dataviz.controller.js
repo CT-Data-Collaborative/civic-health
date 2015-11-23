1e4018ae2b8efad6d92dc6cab767661a71f7961b
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-11-23 14:29:37 -0500

    Initial commit. Basically a skeleton copy of ctdata-angular-template repository, with the addition of /static/dist/data folder that contains raw data, processing scripts, and processed data. Also contains a json of contributor information - to be expanded upon with descriptions later.

---
+++ static/js/dataviz/dataviz.controller.js
@@ -0,0 +1,16 @@
+angular.module('app')
+.controller('DataVizController',
+    ['$scope', '$http', '$log', 'sidebarDisplay', 'categories', 'lodash',
+    function($scope, $http, $log, sidebarDisplay, categories, lodash){
+        var lo = lodash;
+        $scope.toggle = sidebarDisplay.toggle;
+        $scope.categories = categories.list;
+
+        // Sample watch...doesn't do much, but demonstrates how
+        // to watch object from a service.
+        $scope.$watchCollection(function() {
+            return $scope.categories;
+        }, function() {
+            console.log("Selection Detected");
+        }, true);
+}])
