ca5448022b066f495f1c4163dca1474b8dfef634
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-11-23 16:23:50 -0500

    Re-implemented routing and ng-view

--- static/js/routes.js
+++ static/js/routes.js
@@ -1,8 +1,14 @@
 angular.module('app')
-.config(function($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'index.html'
-    }).when('/about.html', {
-        templateUrl: 'about.html'
-    });
+.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/data', {
+            templateUrl: 'static/templates/data.html',
+            controller: 'DataVizController'
+        })
+        .when('/about', {
+            templateUrl: 'static/templates/about.html'
+        })
+        .otherwise({
+            redirectTo: '/data'
+        });
 });
