d432a78617cab2ce03069f0f6137908238139703
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-11-24 09:55:41 -0500

    Added html templates used in routes to content uploaded to s3 box.

--- aws-upload.conf.js
+++ aws-upload.conf.js
@@ -5,6 +5,7 @@ module.exports = {
         "static/dist/js/*.js",
         "static/dist/css/*.css",
         "static/dist/data/*.json",
+        "static/dist/templates/*.html",
         "static/dist/bower_components/**/*.*",
         "index.html"
     ]
