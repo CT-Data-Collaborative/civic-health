44af1d97aaecdc42b71e07f13e44e9f38847dcbf
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-11-25 12:07:50 -0500

    Further work to timeseries d3.js script. Output looks MUCH better. I'd like to make it responsive if it's possible. This commit contains the sass partials as well - will probably augment these as i go and make them more scss-esque.

--- static/dist/js/timeseries.js
+++ static/dist/js/timeseries.js
@@ -16,9 +16,17 @@ function timeSeries() {
         selection.each(function(dataset) {
             //console.log(dataset);
             var svg = d3.select(this).select("svg").remove(),
-                config = dataset["config"],
-                rawdata = dataset["data"],
-                data = rawdata;
+                width = 960, // need to figure out how to get this from the directive, and have it redraw on window size change
+                height = 0.35 * width, // need to figure out how to get this from the directive, and have it redraw on window size change
+                margin = {
+                    top: height * 0.1,
+                    left: width * 0.04,
+                    bottom: height * 0.08,
+                    right: width * 0.03
+                },
+                // config = dataset["config"],
+                // rawdata = dataset["data"],
+                data = dataset;
 
                 if (data.length > 0) {
                     svg = d3.select(this).append("svg")
@@ -31,15 +39,16 @@ function timeSeries() {
                     var parseDate = d3.time.format("%Y").parse;
 
                     var x = d3.time.scale()
-                        .range([0, width]);
+                        .range([10, width]);
 
                     var y = d3.scale.linear()
-                        .range([height, 0]);
+                        .range([height, 10]);
 
                     var color = d3.scale.category10();
 
                     var xAxis = d3.svg.axis()
                         .scale(x)
+                        .ticks(d3.time.years, 4)
                         .orient("bottom");
 
                     var yAxis = d3.svg.axis()
@@ -50,8 +59,7 @@ function timeSeries() {
                         .x(function(d) { return x(d.date); })
                         .y(function(d) { return y(d.value); });
 
-                    var svg = d3.select(this).append('svg')
-                        .attr("width", width + margin.left + margin.right)
+                    var g = svg.attr("width", width + margin.left + margin.right)
                         .attr("height", height + margin.top + margin.bottom)
                       .append("g")
                         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -71,9 +79,8 @@ function timeSeries() {
                         };
                     });
 
-
-                    d3.select(this).append("pre")
-                    .text(JSON.stringify(locations, null, 4))
+                    // d3.select(this).append("pre")
+                    // .text(JSON.stringify(locations, null, 4))
 
                     x.domain(d3.extent(data, function(d) { return d.date; }));
 
@@ -82,22 +89,7 @@ function timeSeries() {
                       d3.max(locations, function(l){ return d3.max(l.values, function(v) {return v.value;}); })
                     ]);
 
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(xAxis);
-
-                    svg.append("g")
-                        .attr("class", "y axis")
-                        .call(yAxis)
-                      .append("text")
-                        .attr("transform", "rotate(-90)")
-                        .attr("y", 6)
-                        .attr("dy", ".71em")
-                        .style("text-anchor", "end")
-                        .text("Participation (%)");
-
-                    var location = svg.selectAll(".location")
+                    var location = g.selectAll(".location")
                         .data(locations)
                       .enter().append("g")
                         .attr("class", "location");
@@ -108,12 +100,69 @@ function timeSeries() {
                         .style("stroke", function(d) { return color(d.name); })
                         .style("fill", "none");
 
-                    location.append("text")
-                        .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-                        .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-                        .attr("x", 3)
-                        .attr("dy", ".35em")
-                        .text(function(d) { return d.name; });
+                    location.selectAll("circles")
+                        .data(function(d) { return d.values.map(function (v) {
+                                v.name = d.name;
+                                return v;
+                            })
+                        })
+                        .enter()
+                        .append("circle")
+                            .style("fill", function(d) { return color(d.name); })
+                            .attr("class", "timepoint")
+                            .attr("cx", function(d) { return x(d.date); })
+                            .attr("cy", function(d) { return y(d.value); })
+                            .attr("r", "3px")
+                        ;
+
+                    // location.append("text")
+                    //     .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+                    //     .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.value) + ")"; })
+                    //     .attr("x", 3)
+                    //     .attr("dy", ".35em")
+                    //     .text(function(d) { return d.name; });
+
+                    g.append("g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(xAxis);
+
+                    g.append("g")
+                        .attr("class", "y axis")
+                        .call(yAxis)
+                      .append("text")
+                        .attr("transform", "rotate(-90)")
+                        .attr("y", 6)
+                        .attr("dy", ".71em")
+                        .style("text-anchor", "end")
+                        .text("Participation (%)");
+
+                    legend = g.append("g")
+                        .attr("height", svg.attr("height") * (d3.keys(locations).length * 0.05))
+                        .attr("width", svg.attr("width") * 0.20)
+                        .attr("transform", "translate(" + width + ", 0)");
+
+                    legend.selectAll("rect")
+                        .data(locations)
+                        .enter()
+                            .append("rect")
+                            .attr("height", 10)
+                            .attr("width", 10)
+                            .attr("x", 0)
+                            .attr("y", function(d, i) { return i * svg.attr("height") * 0.05})
+                            .attr("fill", function(d) { return color(d.name); })
+
+                    legend.selectAll("text")
+                        .data(locations)
+                        .enter()
+                            .append("text")
+                            .text(function(d) { return d.name; })
+                            .attr("x", 0)
+                            .attr("y", function(d, i) { return i * svg.attr("height") * 0.05})
+                            .attr("dx", -5)
+                            .attr("dy", 10)
+                            .attr("text-anchor", "end");
+
                 } else {
                     console.log("no data yet")
                 }
