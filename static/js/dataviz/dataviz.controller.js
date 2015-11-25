7cf58be7b681debf164738005763e3a5cea3a1ef
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-11-25 15:24:50 -0500

    New button to show/hide timeseries, if one exists for a given indicator.

--- static/js/dataviz/dataviz.controller.js
+++ static/js/dataviz/dataviz.controller.js
@@ -1,9 +1,15 @@
 angular.module('app')
 .controller('DataVizController',
-    ['$scope', '$http', '$log', '$location',/* '$anchorScroll', '$rootScope', '$routeParams', */'sidebarDisplay', 'categories', 'lodash',
-    function($scope, $http, $log, $location,/* $anchorScroll, $rootScope, $routeParams, */sidebarDisplay, categories, lodash){
+    ['$scope', '$http', '$log', '$location', '$filter',/* '$anchorScroll', '$rootScope', '$routeParams', */'sidebarDisplay', 'categories', 'lodash',
+    function($scope, $http, $log, $location, $filter,/* $anchorScroll, $rootScope, $routeParams, */sidebarDisplay, categories, lodash){
         var lo = lodash;
         $scope.toggle = sidebarDisplay.toggle;
+
+        $scope.toggleChart = function(slug) {
+            slug = $filter('sluggify')(slug);
+
+            $("#"+slug).toggle();
+        }
 
         // $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
         //     $location.hash($routeParams.scrollTo);
