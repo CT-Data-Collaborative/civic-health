612d04028959719d1a1c40eb6b3fd0f25ef3df9e
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2015-12-02 09:01:58 -0500

    Data for data tables is now using ng-html-bind so that we can trust output as safe html - this is necessary as we also switched from * and NA to dagger and double dagger entities, respectively.

--- static/js/filters.js
+++ static/js/filters.js
@@ -2,9 +2,9 @@ angular.module('app')
 .filter('suppressions', function() {
   return function(input) {
     if (input === "-9,999.0" || input === "-9999") {
-        return 'NA';
+        return '&ddagger;';
     } else if (input === "-666,666.0" || input === "-666666") {
-        return ' * ';
+        return '&dagger;';
     } else {
         return input;
     }
@@ -63,7 +63,5 @@ angular.module('app')
     };
 })
 .filter('safe', ['$sce', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val.toString());
-    };
+    return $sce.trustAsHtml;
 }]);
